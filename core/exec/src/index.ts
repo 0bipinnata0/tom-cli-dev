d94c14ca1bf9146eaca82f9ef8b97f53cf8c942a
light <0bipinnata0@users.noreply.github.com>
2025-02-28 21:15:38 +0800

    实现package

--- core/exec/src/index.ts
+++ core/exec/src/index.ts
@@ -1,15 +1,22 @@
 import logger from "@tom-cli-dev/log";
 import { Package } from "@tom-cli-dev/package";
+
+const SETTINGS = {
+  init: "@tom-cli-dev/init",
+  config: "@tom-cli-dev/config",
+};
+
 export function exec(...args: any[]) {
   const targetPath = process.env.CLI_TARGET_PATH;
   const homePath = process.env.CLI_HOME_PATH;
-  logger.default.info("exec", targetPath, homePath);
+  logger.info("exec" + "__" + targetPath + "__" + homePath);
   const cmdObj = args[args.length - 1];
   const pkgName = cmdObj.name();
   const pkgVersion = "latest";
   const pkg = new Package({
     targetPath,
-    pkgName,
+    pkgName: SETTINGS[pkgName as keyof typeof SETTINGS],
     pkgVersion,
   });
+  logger.info(pkg);
 }
